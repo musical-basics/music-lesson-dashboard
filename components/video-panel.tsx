33b99fb063f24967da0235d7ee7ab01cc8e8553e
Lionel Yu <support@musicalbasics.com>
2026-02-22 18:10:19 -0800

    Fix dual-view teacher video getting cut off on small screens

--- components/video-panel.tsx
+++ components/video-panel.tsx
@@ -100,7 +100,7 @@ function VerticalVideoStack({ aspectRatio = "standard" }: { aspectRatio?: VideoA
             {tracks.map((track) => (
                 <div
                     key={track.participant.identity}
-                    className={`relative overflow-hidden flex-1 min-h-0 ${videoStyleClass}`}
+                    className={`relative overflow-hidden flex-1 min-h-0 max-h-[50%] ${videoStyleClass}`}
                     style={getContainerStyle()}
                 >
                     <ParticipantTile
